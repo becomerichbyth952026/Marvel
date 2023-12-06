30994d8d3110190804ba1425a63bfd17df965977
becomerichbythe952026 <nl91alessandro@gmail.com>
2023-12-06 12:30:50 +0100

    Initial commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import json from 'marvel.json'
+
+
+export function Main(){
+  return (
+    <div className="header">
+      <div className="bg">
+        <img src="" alt="" />
+      </div>
+      <div className="search-bar">
+        
+      </div>
+    </div>
+  );
+}
