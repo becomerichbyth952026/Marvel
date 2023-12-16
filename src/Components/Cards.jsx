6278e53796d0fd031f2ea87b10b8877d5516d95f
becomerichbythe952026 <nl91alessandro@gmail.com>
2023-12-16 17:24:42 +0100

    cards

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import Hulk from '../media/hulk.jpg';
+import React from "react";
 
- 
-export const Cards = () => {
+export const Cards = ({ data }) => {
   return (
-
     <>
-
-      <div className='card'>
-
-        <img src={Hulk} alt="" />
-        <div className="card-content">
-          <h3>Hulk</h3>
-        </div>
-      </div>
-
-     
-
-    
-    
+      {data
+        ? data.map((item) => {
+            return (
+              <div className="card">
+                <img
+                  src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                  alt=""
+                />
+                <div className="card-content">
+                  <h3>Hulk</h3>
+                </div>
+              </div>
+            );
+          })
+        : ""}
     </>
-    
   );
-}
+};
 
-export default Cards
+export default Cards;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,40 +1,42 @@
 import React from "react";
-import MarvelImg from '../media/187551-captain-america-1440x829.jpg'
-import MarvelLogo from '../media/logo.jpg'
+import MarvelImg from "../media/187551-captain-america-1440x829.jpg";
+import MarvelLogo from "../media/logo.jpg";
 import { Cards } from "./Cards";
-
-
-
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const Main = () => {
+  const [url, setUrl] = useState(
+    "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=c6af68b73c58eab3d79df3489d7f849c&hash=db3748ad2119e262ea8f7cdf6199d575"
+  );
+  const [item, setItem] = useState();
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await axios.get(url);
+      setItem(res.data.data.results);
+    };
+    fetch();
+  }, [url]);
   return (
     <>
-    
-    <div className="header">
-      
-      <div className="py">
-        <img src={MarvelImg} alt="" />
+      <div className="header">
+        <div className="py">
+          <img src={MarvelImg} alt="" />
+        </div>
+        <div className="search-bar">
+          <img src={MarvelLogo} alt="logo" />
+          <input
+            type="search"
+            placeholder="Search Information"
+            className="search"
+          />
+        </div>
+
+        <div className="content">
+          {!item ? <p>Not Found</p> : <Cards data={item} />}
+        </div>
       </div>
-      <div className="search-bar">
-        <img src={MarvelLogo} alt="logo"/>
-        <input type="search" placeholder="Search Information" 
-        className="search"/>
-      </div>
-
-      <div className="content">
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-
-      </div>
-
-      
-    </div>
-    
-
-    
     </>
   );
-}
+};
